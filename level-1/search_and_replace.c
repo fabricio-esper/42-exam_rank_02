02e6e7fc98ba4748194d2b25e293b0672a8e5feb
fesper-s <fafaeesfabricio@gmail.com>
2022-08-03 14:55:51 -0300

    added search_and_replace.c

---
+++ level-1/search_and_replace.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc == 4)
+	{
+		i = 0;
+		if (!argv[2][1] && !argv[3][1])
+		{
+			while (argv[1][i])
+			{
+				if (argv[1][i] == argv[2][0])
+					argv[1][i] = argv[3][0];
+				write(1, &argv[1][i], 1);
+				i++;
+			}
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
